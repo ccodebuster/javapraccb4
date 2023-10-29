4fe10c5440ec3c80007675652f1a42b32ba1ccf4
padivyab <divyapatel.dp908@gmail.com>
2023-10-29 12:39:13 -0400

    divya

---
+++ javademo/src/gitpractice/Divya.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Divya {
+    public static void main(String[] args) {
+        System.out.println("Git is a version control tool");
+        System.out.println("how to create branch");
+    }
+}
