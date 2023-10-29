4c44978c118881ec77721c1cd70fc740be73a8b7
Rushit93 <rushitp93@gmai.com>
2023-10-29 16:38:55 +0000

    Rushit-WK-08

---
+++ javademo/src/gitpractice/Rushit.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Rushit {
+    public static void main(String[] args) {
+        System.out.println("India beats England by 1oo Runs");
+        System.out.println("It's coming home");
+    }
+}
