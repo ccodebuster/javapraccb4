963bfcc09ca2a031856ca37739c5479f13935c3a
YamahaNmax <m4mitzlearning@gmail.com>
2023-10-29 16:38:57 +0000

    Miten-WK-08

---
+++ javademo/src/gitpractice/Miten.java
@@ -0,0 +1,15 @@
+package gitpractice;
+
+public class Miten {
+
+    public static void main(String[] args) {
+        print();
+
+    }
+
+    static void print(){
+
+        System.out.println("Hey there, How's your day?");
+        System.out.println("Let me know, If you need any help. ?");
+    }
+}
