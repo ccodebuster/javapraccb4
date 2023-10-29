b325bd58dbb7825e86dad19f1462c8b5a67ddaf6
tracksafe1 <shikha8883@gmail.com>
2023-10-29 16:38:41 +0000

    Shikha-wk-8

---
+++ javademo/src/gitpractice/Shikha.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Shikha {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("Will there be a class on Diwali as well");
+    }
+}
