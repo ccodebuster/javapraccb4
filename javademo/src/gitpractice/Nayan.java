422c6c66025b3283476517d2b127d5b552dc8882
NAYANQA <nayanqa.om@gmail.com>
2023-10-29 16:39:11 +0000

    Nayan08

---
+++ javademo/src/gitpractice/Nayan.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Nayan {
+    public static void main(String[] args) {
+        System.out.println("Java code is updated");
+        System.out.println("not code is updated");
+    }
+}
