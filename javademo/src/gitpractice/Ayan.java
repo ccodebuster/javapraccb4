8ba2deda90f0084fb9dc53ca41b638f26d59464d
MysteriousRomeo <roy.ayan.roy@gmail.com>
2023-10-29 16:38:19 +0000

    ayan wk 08

---
+++ javademo/src/gitpractice/Ayan.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Ayan {
+    public static void main(String[] args) {
+        System.out.println("I am here, making a public statement that I am bored as F :P. " +
+                            "\nAlso India won the match today against England by 100 runs :D");
+    }
+}
