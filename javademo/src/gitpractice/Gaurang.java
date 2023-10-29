424f9885ad437c9a68c4029d604b2234293c5331
bhav3583 <bhavesh3583@yahoo.co.in>
2023-10-29 16:39:10 +0000

    Gaurang-WK-08

---
+++ javademo/src/gitpractice/Gaurang.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Gaurang {
+    public static void main(String[] args) {
+        System.out.println("I am loving a Git");
+        System.out.println("I know now how to create new branch");
+    }
+}
