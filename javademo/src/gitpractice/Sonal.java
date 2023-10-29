9a96339da3dcd534c30cc6cfcaeec371a66da2c9
vinihoney <sonessel@gmail.com>
2023-10-29 16:38:22 +0000

    Soanl-Wk-08

---
+++ javademo/src/gitpractice/Sonal.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Sonal {
+    public static void main(String[] args){
+        System.out.println("Git Practice");
+        System.out.println("Git Enhance ");
+    }
+}
