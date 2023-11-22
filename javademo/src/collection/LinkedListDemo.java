98b07fe76e842cc260623801849d34e95238b9eb
bhav3583 <bhavesh3583@yahoo.co.in>
2023-11-22 21:55:19 +0000

    collection

--- javademo/src/collection/LinkedListDemo.java
+++ javademo/src/collection/LinkedListDemo.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/*
+* Java LinkedList class can contain duplicate elements.
+Java LinkedList class maintains insertion order.
+Java LinkedList class is non synchronized.
+In Java LinkedList class, manipulation is fast because no shifting needs to occur.
+Java LinkedList class can be used as a list, stack or queue.*/
+
 public class LinkedListDemo<S> {
     public static void main(String[] args) {
 
