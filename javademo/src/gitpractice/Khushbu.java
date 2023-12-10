24974078a7faa69e5b5d234f917c99a53303660a
Pastry2802 <khushbu2827@gmail.com>
2023-12-10 16:08:22 +0000

    khushbu14

--- javademo/src/gitpractice/Khushbu.java
+++ javademo/src/gitpractice/Khushbu.java
@@ -1,9 +1,16 @@
 package gitpractice;
 
 public class Khushbu {
+    String name;
     public static void main(String[] args) {
         System.out.println("Eating good food is my favourite hobby");
         System.out.println("I love cooking good food");
         System.out.println("Where there is a WILL, there is a way!");
+        Khushbu khushbu = new Khushbu("Khushbu");
+        System.out.println(khushbu);
     }
+     Khushbu(String name){
+        this.name = name;
+    }
+
 }
