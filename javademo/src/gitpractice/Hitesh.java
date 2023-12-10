4fe3f530e1d342e6c2df60ac8b481f249d2615a4
NAYANQA <nayanqa.om@gmail.com>
2023-12-10 16:58:38 +0000

    updated hitesh

--- javademo/src/gitpractice/Hitesh.java
+++ javademo/src/gitpractice/Hitesh.java
@@ -4,6 +4,9 @@ public class Hitesh {
     public static void main(String[] args) {
         System.out.println("I Like Java");
         System.out.println("I Dream Java");
+        System.out.println("I Dream Java");
+        System.out.println("I Dream Java");
+        System.out.println("I Dream Java");
 
     }
 }
