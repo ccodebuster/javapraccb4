dfb65f133d8360c46c80e0d7ea4a4d018aa2c429
Rushit93 <rushitp93@gmail.com>
2023-12-10 16:08:29 +0000

    Rushit13

---
+++ javademo/src/gitpractice/Rushit.java
@@ -0,0 +1,18 @@
+package gitpractice;
+
+public class Rushit {
+
+    String name = "Rushit";
+
+    public static void main(String[] args) {
+        Rushit rushit=new Rushit("Rushit");
+        //System.out.println(rushit);
+    }
+
+    public Rushit (String name) {
+        this.name = name;
+
+        System.out.println(name);
+
+    }
+}
