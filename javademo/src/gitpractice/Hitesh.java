25dfde612dc286b543a71ed99456be27a47de9fb
[name] <destiny.creator1959@gmail.com>
2023-12-10 16:08:30 +0000

    Hitesh009

--- javademo/src/gitpractice/Hitesh.java
+++ javademo/src/gitpractice/Hitesh.java
@@ -1,9 +1,18 @@
 package gitpractice;
-
+;
 public class Hitesh {
+    String name;
     public static void main(String[] args) {
+        Hitesh hitesh = new Hitesh( ("Patel") );
         System.out.println("I Like Java");
         System.out.println("I Dream Java");
+        System.out.println(hitesh);
+
 
     }
+    Hitesh(String name){
+        this.name=name;
+
+    }
+
 }
