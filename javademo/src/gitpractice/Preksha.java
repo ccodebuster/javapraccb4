acbc90894d49f0397a806ce2bc896c6f0fb0b727
austinflower <patelpreksha2008@gmail.com>
2023-12-10 10:09:32 -0600

    Preksha

--- javademo/src/gitpractice/Preksha.java
+++ javademo/src/gitpractice/Preksha.java
@@ -1,8 +1,12 @@
 package gitpractice;
 
 public class Preksha {
-
+    String name;
     public static void main(String []args){
+        Preksha preksha = new Preksha("Patel");
         System.out.println("Preksha is using Git");
+    }
+    Preksha (String name){
+        this.name="name";
     }
 }
