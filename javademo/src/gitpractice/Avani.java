f9d06ead2dd978bc7ec16093517c7a1925ca8a56
avani244@ <patelavani244@gmail.com>
2023-12-10 16:08:20 +0000

    a

---
+++ javademo/src/gitpractice/Avani.java
@@ -0,0 +1,14 @@
+package gitpractice;
+
+public class Avani {
+String name;
+    public static void main(String[] args) {
+        Avani x = new Avani("Git practice");
+        System.out.println(x);
+
+    }
+    Avani (String name){
+        this.name=name;
+
+    }
+}
