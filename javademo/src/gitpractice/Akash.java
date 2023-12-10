99c8161f6c9425d048bae36907c91ffd7571de83
mysteriousromeo <roy.ayan.roy@gmail.com>
2023-12-10 16:09:50 +0000

    pushing

--- javademo/src/gitpractice/Akash.java
+++ javademo/src/gitpractice/Akash.java
@@ -5,4 +5,8 @@ public class Akash {
     public static void main(String[] args) {
         System.out.println("i am loving a java");
     }
+
+    public static void ayan09(){
+        System.out.println("I am the first one here! after akash!");
+    }
 }
---
+++ javademo/src/gitpractice/Ayan.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class Ayan {
+
+    public static void main(String[] args) {
+        System.out.println("ayan");
+    }
+
+
+}
