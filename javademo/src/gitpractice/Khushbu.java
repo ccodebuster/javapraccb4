9fadf7ab059b0105dbaa526541b6e7599687b53e
Pastry2802 <khushbu2827@gmail.com>
2024-01-28 15:14:24 +0000

    Khushbu2808

--- javademo/src/gitpractice/Khushbu.java
+++ javademo/src/gitpractice/Khushbu.java
@@ -3,7 +3,19 @@ package gitpractice;
 public class Khushbu {
     public static void main(String[] args) {
         System.out.println("Eating good food is my favourite hobby");
+        System.out.println("Eating good food is my favourite hobby");
+        System.out.println("Eating good food is my favourite hobby");
         System.out.println("I love cooking good food");
+        System.out.println("I love cooking good food");
+        System.out.println("I love cooking good food");
+        System.out.println("I love cooking good food");
+        System.out.println("I love cooking good food");
+        System.out.println("Where there is a WILL, there is a way!");
+        System.out.println("Where there is a WILL, there is a way!");
+        System.out.println("Where there is a WILL, there is a way!");
+        System.out.println("Where there is a WILL, there is a way!");
+        System.out.println("Where there is a WILL, there is a way!");
+        System.out.println("Where there is a WILL, there is a way!");
         System.out.println("Where there is a WILL, there is a way!");
     }
 }
