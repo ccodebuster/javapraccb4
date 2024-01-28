0fcdb3fbdb4ac53f07b2c2edee26ac6ee3c09042
shubhushubhi <kinnari1590@gmail.com>
2024-01-28 15:12:36 +0000

    Kinnari12

--- javademo/src/gitpractice/Kinnari.java
+++ javademo/src/gitpractice/Kinnari.java
@@ -4,5 +4,11 @@ public class Kinnari {
     public static void main(String[] args) {
         System.out.println("I love India");
         System.out.println("I know how to create new branch");
+        System.out.println("I know how to create new branch");
+        System.out.println("I know how to create new branch");
+        System.out.println("I know how to adding more code");
+        System.out.println("start getting job");
+        System.out.println("practice practice");
+
     }
 }
