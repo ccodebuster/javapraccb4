3c3eb9c1a699c37a62749f9ebccdaf4ef6aacf90
GUILDFORD123 <riptipatel11@gmail.com>
2024-01-28 15:19:08 +0000

    riptipatel

--- javademo/src/gitpractice/Riptipatel.java
+++ javademo/src/gitpractice/Riptipatel.java
@@ -1,7 +1,6 @@
 package gitpractice;
 
-public class Ripti {
-
+public class Riptipatel {
     public static void main(String[] args) {
         System.out.println("i love my self");
         System.out.println("interview done ");
