f3382d70f4cc9d379dac84f31fca9f6498d44df3
YamahaNmax <m4mitzlearning@gmail.com>
2024-01-28 15:12:26 +0000

    Miten7652

--- javademo/src/gitpractice/Bhavesh.java
+++ javademo/src/gitpractice/Bhavesh.java
@@ -4,6 +4,22 @@ public class Bhavesh {
 
     public static void main(String[] args) {
         System.out.println("lets rock");
+        System.out.println("Hey!! Man");
+        System.out.println("HIp Hip Hoorey We gonna start appying the jobs now");
+        System.out.println("I will do group study all the questions everyday");
+        System.out.println("Practice, --------");
+        System.out.println("Practice, --------");
+        System.out.println("Practice, --------");
+        System.out.println("Practice, --------");
+        System.out.println("Practice, --------");
+        System.out.println("Practice, --------");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
+        System.out.println("lets rock");
         System.out.println("adding more code");
         System.out.println("adding more cod1");
 
