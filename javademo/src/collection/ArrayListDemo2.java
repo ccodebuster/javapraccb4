ceef2201002e4ab035dc7fbbf99aa507c7b8cfa1
padivyab <divyapatel.dp908@gmail.com>
2024-01-28 10:19:36 -0500

    di

--- javademo/src/collection/ArrayListDemo2.java
+++ javademo/src/collection/ArrayListDemo2.java
@@ -21,5 +21,4 @@ public class ArrayListDemo2<S> {
 
     }
 
-
 }
---
+++ javademo/src/gitpractice/Divya.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Divya {
+
+    public static void main(String[] args) {
+        System.out.println("divya");
+    }
+}
